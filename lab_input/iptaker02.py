68f638e705432973e372539922199c2bd1a435c4
NA-Snyder <nsnyder1994@gmail.com>
2020-12-07 22:41:12 +0000

    "Adding user_input string"

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+
+## the line below creates a single string that is passed to print()
+# print("YOu told me the IPv4 address is:" + user_input)
+
+## print() can be given a series of objects separated by a comma
+print("You told me the IPv4 address is:", user_input)
+
+##line asks for input asking for vender name
+vender_name = input("Please enter the vender name for the device:")
+
+## print returns vender name input in string
+print("You told me the vender name is:", vender_name)
