7a6fb4396241395e494b47e7d910db9dd68c508a
NA-Snyder <nsnyder1994@gmail.com>
2020-12-09 19:27:53 +0000

    custom if-logic script

---
+++ custif/mycustif.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+age = int(input("How old are you?"))
+
+if age < 10:
+    print("You are younger than 10.")
+
+elif age < 25:
+    print("You are younger than 25.")
+
+elif age < 50:
+    print("You are younger that 50.")
+
+else:
+    print("You are 50 or older.")
+
+print("Exiting script")
+
+
+
