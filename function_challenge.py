1214dbc90a62cfa7150c553404f4e455efd88623
NA-Snyder <nsnyder1994@gmail.com>
2020-12-11 17:01:50 +0000

    function challange

---
+++ function_challenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def hello(name):
+    print("Hello " + name)
+    return
+hello(input("What is your name?"))
+
+def multiply(number):
+    newnumber = number * 11 # not actually multiplying number by 11, just printing number 11 times.
+    print(newnumber)
+    return
+multiply(input("Type a number."))
+
